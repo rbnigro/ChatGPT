bd44a3962379159268e4abe531255e727bb526f7
Ronney Nigro <38004166+rbnigro@users.noreply.github.com>
2023-05-12 11:22:28 -0300

    Reconhecimento por voz

--- ChatGPT/src/main/java/br/com/ronney/chatGPT/Principal.java
+++ ChatGPT/src/main/java/br/com/ronney/chatGPT/Principal.java
@@ -1,78 +1,14 @@
 package br.com.ronney.chatGPT;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Properties;
-import java.util.Scanner;
-
-import br.com.ronney.entity.Model;
-import br.com.ronney.methods.ChatGPT;
-import br.com.ronney.methods.ChatText;
+import br.com.ronney.tipoChat.ChatPorTexto;
 
 public class Principal {
 
-	// https://platform.openai.com/account/api-keys
-	private static String API_KEY;
-	
-	private static String sUserInput = "";
-	private static String sInitPrompt = "";
-	private static String sQuestionDialog = "";
-	private static String response = "";
-	
 	//  quero testar minha aplicação que estou fazendo para integrar com o chatGPT
 	public static void main(String[] args) throws IOException {
-		String sTipo = "l";
-		
-		int iPassagens = 1;
-		
-		Properties properties = new Properties();
-		FileInputStream fileInputStream = new FileInputStream("./properties/conf.properties");
-		properties.load(fileInputStream);
-		API_KEY = properties.getProperty("openai.apikey");
-		
-		Scanner scannerPrompt = new Scanner(System.in);
-		
-		while (iPassagens <= 3) {
-			System.out.print("Você:");
-			sUserInput = scannerPrompt.nextLine();
-		
-			sInitPrompt += sUserInput + "\nChatbot:";
-		
-			System.out.println("Aguarde...");
-		
-			if (!sUserInput.isEmpty())
-			{
-				sQuestionDialog = sUserInput + sInitPrompt;
-				response = chamadaChatGPT(sQuestionDialog, sTipo);
-			}
-			
-			System.out.println(response);
-			
-			sInitPrompt += response + "\n";
-			 
-			iPassagens++;
-		}
-		
-		scannerPrompt.close();
+		ChatPorTexto.chatPorTexto();
 	}
 
-	private static String chamadaChatGPT(String promptScanner, String pTipo) { 
 
-		ChatGPT methodsChatGPT;
-		ChatText methodsChatText;
-		
-		String retorno = "<vazio>";
-		
-		if (pTipo.toUpperCase().equals("C")) {
-			methodsChatText = new ChatText(API_KEY);
-			retorno = methodsChatText.askText(Model.TEXT_DAVINCI_003.getName(), promptScanner);
-		}
-		
-		if (pTipo.toUpperCase().equals("L")) {
-			methodsChatGPT = new ChatGPT(API_KEY);
-			retorno = methodsChatGPT.askGPT(Model.GPT_3_5_TURBO_0301.getName(), promptScanner);
-		}
-		
-		return retorno;
-	}
 }
--- ChatGPT/src/main/java/br/com/ronney/entity/service/ConversationService.java
+++ ChatGPT/src/main/java/br/com/ronney/entity/service/ConversationService.java
@@ -1,10 +1,35 @@
 package br.com.ronney.entity.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import br.com.ronney.model.Conversation;
+import br.com.ronney.repository.ConversationRepository;
 
 @Service
 @Transactional
 public class ConversationService {
+	
+    @Autowired
+    private ConversationRepository conversationRepository;
+    
+    public List<Conversation> getAllConversations() {
+        return conversationRepository.findAll();
+    }
+    
+    
+    public Conversation getConversationById(Long id) {
+        return conversationRepository.findByIdconversation(id);
+    }
 
+    public Conversation saveConversation(Conversation conversation) {
+        return conversationRepository.save(conversation);
+    }
+ 
+    public void deleteConversationById(Long id) {
+        conversationRepository.deleteByIdconversation(id);
+    }
 }
